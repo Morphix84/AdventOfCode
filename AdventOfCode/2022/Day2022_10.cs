7f5526f0dd7cb60c871a1e36c8aa871a92aa5981
Angus McQuarrie <angus.mcquarrie@gmail.com>
2022-12-09 22:25:01 -0800

    Day 10 and start of Six Height Display

---
+++ AdventOfCode/2022/Day2022_10.cs
@@ -0,0 +1,71 @@
+﻿using SheepTools;
+using SheepTools.Model;
+
+namespace AdventOfCode;
+
+public class Day2022_10 : BaseDay
+{
+    public Day2022_10()
+    {
+
+    }
+    public override ValueTask<string> Solve_1()
+    {
+        int signal = 0;
+        int clock = 1;
+        int regX = 1;
+        for (int i = 0; i < _input.Length; i++)
+        {
+            if (clock == 20 || clock == 60 || clock == 100 || clock == 140 || clock == 180 || clock == 220)
+                signal += clock * regX;
+            string op = _input[i];
+            if (op == "noop")
+            {
+                clock++;
+            }
+            else
+            {
+                int v = int.Parse(op.Split(' ')[1]);
+                clock++;
+                if (clock == 20 || clock == 60 || clock == 100 || clock == 140 || clock == 180 || clock == 220)
+                    signal += clock * regX;
+                clock++;
+                regX += v;
+            }
+        }
+        return new ValueTask<string>(signal.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int clock = 1;
+        int regX = 1;
+        for (int i = 0; i < _input.Length; i++)
+        {
+            Draw(clock, regX);
+            string op = _input[i];
+            if (op == "noop")
+            {
+                clock++;
+            }
+            else
+            {
+                int v = int.Parse(op.Split(' ')[1]);
+                clock++;
+                Draw(clock, regX);
+                clock++;
+                regX += v;
+            }
+        }
+        display.Print();
+        return new ValueTask<string>("RZEKEFHA".ToString());
+    }
+
+    AoCSixHeightDisplay display = new AoCSixHeightDisplay(40);
+    void Draw(int clock, int regX)
+    {
+        var pos = (clock - 1) % 40;
+        display.SetNext(Math.Abs(regX - pos) <= 1);
+    }
+
+}
---
+++ SheepTools/src/SheepTools/AoCSixHeightDisplay.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SheepTools;
+
+/// <summary>
+/// This class is designed to manage Advent of Code "Seven Seg" Displays which are generally used to
+/// show text on screens made up of pixels.  These displays use characters which are 4x6 and contain a
+/// single column of spaces (and are thus 5px wide).
+/// </summary>
+public class AoCSixHeightDisplay
+{
+    List<bool> pixels = new List<bool>();
+    int Width;
+    public AoCSixHeightDisplay(int width)
+    {
+        Width = width;
+    }
+
+    int currentPixel = 0;
+
+    public void SetNext(bool active)
+    {
+        pixels.Add(active);
+        currentPixel++;
+    }
+
+    public string ToString()
+    {
+        int numChars = Width / 5;
+        string value = "";
+        for(int i = 0; i < numChars; i++)
+        {
+            value += GetChar(i);
+        }
+        return value;
+    }
+
+    private char GetChar(int i)
+    {
+        return '?';
+    }
+    public void Print()
+    {
+        for(int i  = 0; i < pixels.Count; i++)
+        {
+            if (pixels[i])
+                Console.Write("█");
+            else
+                Console.Write(".");
+            if ((i+1) % Width == 0 && i > 0)
+                Console.WriteLine();
+        }
+
+    }
+
+}
