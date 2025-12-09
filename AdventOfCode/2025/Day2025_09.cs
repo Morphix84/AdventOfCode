48af89f1f913002652e35889330eef6b3fb71062
Angus McQuarrie <amcquarrie@riotgames.com>
2025-12-09 10:14:35 -0800

    2025 Day 9 stub

---
+++ AdventOfCode/2025/Day2025_09.cs
@@ -0,0 +1,38 @@
+﻿using SheepTools.Extensions;
+using System;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public class Day2025_09 : BaseDay
+{
+
+   
+    public Day2025_09()
+    {
+        ParseInput();
+    }
+    private void ParseInput()
+    {
+
+        
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        int sum = 0;
+
+        return new ValueTask<string>(sum.ToString());
+    }
+
+   
+
+    public override ValueTask<string> Solve_2()
+    {
+        int sum = 0;
+
+        return new ValueTask<string>(sum.ToString());
+    }
+}
